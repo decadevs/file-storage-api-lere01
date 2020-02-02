614bd447cf33716e8fb6d79f21ff40f222cdd347
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-02 03:14:19 +0100

    create serializer class

---
+++ api/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Bucket
+
+class BucketSerializer(ModelSerializer):
+    class Meta:
+        model = Bucket
+        fields = ['id', 'name']
