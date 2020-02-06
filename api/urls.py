537e6f8bfd322d35f03429560edce5c759a19b61
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-06 08:45:59 +0100

    adjust urls.py script

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patterns
-from .views import Buckets
-from .views import BucketDetails
+from .views import Buckets, GetBucketDetail
+# from .views import BucketDetails
+
 
 # app_name = 'bucket'
 urlpatterns = [
     path('', Buckets.as_view(), name="create"),
-    path('bucket/<int:pk>', BucketDetails.as_view(), name="detail"),
+    path('bucket/<int:bucket_id>', GetBucketDetail.as_view(), name="detail"),
 ]
+
+
